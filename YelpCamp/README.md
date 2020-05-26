69a56b469a066c6f86d8b303b4e032c7ded82e0e
Shiyu <hshiyu95@gmail.com>
2020-05-25 20:47:29 -0400

    data persistence done

--- YelpCamp/README.md
+++ YelpCamp/README.md
@@ -19,6 +19,17 @@
 * Style the Navbar and Form
 	* Add a navbar to all templates
 	* Style the new campground form
+#v2
+* Add Mongoose
+	* Install and configure mongoose
+	* Setup campground model
+	* Use camoground model inside of our routes!
+* Show Page
+	* Review the RESTful routes we've seen so far
+	* Add description to our campground model
+	* Show db.collection.drop()
+	* Add a show route/template
+* 
 	
 
 
--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,25 +1,51 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
 
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+	name:String,
+	image:String,
+	description:String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+// 	{name:"Granite Hill", 
+// 	image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547849762f7cd29649_340.jpg",
+// 	description:"This is a huge granite hill, no bathroom, no water, beautiful place "
+// },function(err,campground){
+//     if(err){
+//     	console.log(err);
+
+//     }else{
+//     	console.log("Save a campground");
+//     	console.log(campground);
+//     }
+// });
 
 
-var campgrounds =[
-	{name:"Salmon Creek", image:"https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e507440752c7bd3914cc1_340.jpg"},
-	{name:"Granite Hill", image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547849762f7cd29649_340.jpg"},
-	{name:"Salmon Creek", image:"https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e507440752c7bd3914cc1_340.jpg"},
-	{name:"Granite Hill", image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547849762f7cd29649_340.jpg"},
-	{name:"Salmon Creek", image:"https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e507440752c7bd3914cc1_340.jpg"},
-	{name:"Granite Hill", image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547849762f7cd29649_340.jpg"},
+
+
+// var campgrounds =[
+// 	{name:"Salmon Creek", image:"https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e507440752c7bd3914cc1_340.jpg"},
+// 	{name:"Granite Hill", image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547849762f7cd29649_340.jpg"},
+// 	{name:"Salmon Creek", image:"https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e507440752c7bd3914cc1_340.jpg"},
+// 	{name:"Granite Hill", image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547849762f7cd29649_340.jpg"},
+// 	{name:"Salmon Creek", image:"https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e507440752c7bd3914cc1_340.jpg"},
+// 	{name:"Granite Hill", image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547849762f7cd29649_340.jpg"},
 	
-	{name:"Salmon Creek", image:"https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e507440752c7bd3914cc1_340.jpg"},
-	{name:"Granite Hill", image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547849762f7cd29649_340.jpg"},
-	{name:"Mountain Goat's Rest", image:"https://pixabay.com/get/57e1d14a4e52ae14f1dc84609620367d1c3ed9e04e507440752c7bd3914cc1_340.jpg"}	
-	];
+// 	{name:"Salmon Creek", image:"https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e507440752c7bd3914cc1_340.jpg"},
+// 	{name:"Granite Hill", image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547849762f7cd29649_340.jpg"},
+// 	{name:"Mountain Goat's Rest", image:"https://pixabay.com/get/57e1d14a4e52ae14f1dc84609620367d1c3ed9e04e507440752c7bd3914cc1_340.jpg"}	
+// 	];
 
 
 
@@ -27,30 +53,65 @@ app.get("/",function(req,res){
  res.render("landing");
 });
 
-
+//Index - show a list of campgrounds
 app.get("/campgrounds",function(req,res){
-	
+	//get all campgrounds from db
 
-	res.render("campgrounds",{campgrounds:campgrounds});
+Campground.find({},function(err,allCampgrounds){
+	if(err){
+		console.log("ERROR");
+		console.log(err);
+	}else{
+		res.render("index",{campgrounds:allCampgrounds});
+	}
+});
+
+	
 });
 
 
-
+//Create - add a new campground to DB
 app.post("/campgrounds", function(req,res){
 	//get data form form to array
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image}
-	campgrounds.push(newCampground);
+	var desc = req.body.description;
+	var newCampground = {name: name, image: image, description:desc}
+	//campgrounds.push(newCampground);
+
+	//create a new campground and save to db
+	Campground.create(newCampground,function(err,newlyCreated){
+		if(err){
+			console.log(err);
+		}else{
+			res.redirect("/campgrounds");
+		}
+	});
 	//redirect back to campgrounds page
-	res.redirect("/campgrounds");
+	//res.redirect("/campgrounds");
 
 });
 
-
+//NEW --display form to make a new campground
 app.get("/campgrounds/new", function(req,res){
 	res.render("new");
 });
+
+//ShOW -- show more information about campground
+
+app.get("/campgrounds/:id",function(req,res){
+	//find the campground with provided ID
+	Campground.findById(req.params.id, function(err,foundCampground){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("show",{campground:foundCampground});
+			//render show template with that campground
+
+		}
+	});
+})
+
 
 
 app.listen(3000,function(){
