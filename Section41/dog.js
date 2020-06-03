e9ae96782c45cf78ee97616f8e4c51e431b604ae
Shiyu <hshiyu95@gmail.com>
2020-06-03 17:08:39 -0400

    done

---
+++ Section41/dog.js
@@ -0,0 +1,14 @@
+//construction function
+function Dog(name, age){
+   this.name = name;
+   this.age = age
+   this.bark =function(){
+    console.log(this.name + "just barked!");
+}
+};
+
+var rusty = new Dog("Rusty",3);
+var fido = new Dog("Fido",11);
+
+rusty.bark();
+fido.bark();
